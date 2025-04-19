ce10d0197e2ead225fa892bd3ca4663b28e75414
whispers-of-enma <rcj.dev@proton.me>
2025-04-19 10:02:54 +0200

    feat: add recon script for single host icmp ping

---
+++ scripts/recon/icmp_scan.py
@@ -0,0 +1,45 @@
+import subprocess
+import argparse
+
+import utils.validation as validation
+import utils.pretty_print as print
+
+from core.record import singleton as record
+
+input_types = {
+    'target_ip': str,
+    'timeout': int,
+    'count': int, 
+}
+
+@validation.function_types(input_types)
+def run(target_ip: str, timeout: int = 1, count: int = 1) -> bool:
+    command = ['ping', '-c', str(count), '-W', str(timeout), target_ip]
+    result = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
+    
+    for line in result.stdout:
+        if 'ttl=' in line.lower():
+            ttl = int(line.split('ttl=')[1].split()[0])
+            record.add_host_spec(target_ip, 'ttl', ttl)
+            return True
+
+    return False   
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ping single host')
+    parser.add_argument('target_ip', help='target IP address')
+    parser.add_argument('-w', nargs='?', default=1, help='seconds to wait for reply', type=int, metavar='wait', dest='timeout')
+    parser.add_argument('-c', nargs='?', default=5, help='amount of pings to send.', type=int, metavar='count', dest='count')
+    args = parser.parse_args()
+
+    result = run(
+        target_ip=args.target_ip,
+        timeout=args.timeout,
+        count=args.count,
+    )
+
+    if result:
+        print.info(f'Host {args.target_ip} is reachable (TTL = {record.get_host_spec(args.target_ip, 'ttl')})')
+    else:
+        print.info(f'Host {args.target_ip} is unreachable.')
+
