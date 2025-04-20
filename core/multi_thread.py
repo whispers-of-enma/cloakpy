e565eda48f3e647cbbdd27df62a3a9155c8a62ef
whispers-of-enma <rcj.dev@proton.me>
2025-04-20 10:24:49 +0200

    feat: add routine for syn scanning one or many addresses

--- core/multi_thread.py
+++ core/multi_thread.py
@@ -1,13 +1,15 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-def dipatch_cases(task, cases: list[dict], max_threads: int, timeout: int, **kwargs):
+def dipatch_cases(task, cases: list[dict], max_threads: int, **kwargs):
     results = []
     with ThreadPoolExecutor(max_workers=max_threads) as executor:
-        futures = [
-            executor.submit(task, **case, **kwargs) 
+        futures = {
+            executor.submit(task, **case, **kwargs): case
             for case in cases
-        ]
+        }
         for future in as_completed(futures):
-            results.append(future.result())
+            case = futures[future]
+            if future.result():
+                results.append(case)
     
     return results
--- routines/recon/syn_scan_address.py
+++ routines/recon/syn_scan_address.py
@@ -2,19 +2,22 @@ import argparse
 
 import itertools
 
-from utils import validation
-from utils import pretty_print as print
-
 from core import multi_thread as multithread
 
 from tasks.recon import syn_scan
 
-def run(target_network_ip: str, target_network_mask: int | None = None, target_ports: list | int | str | None = None, max_threads: int = 100, timeout: int = 1) -> list[str]:
-    target_ips_list, target_network_ip, target_network_mask = validation.validate_ip_network(target_network_ip, target_network_mask)
+from utils import pretty_print as print
+from utils import validation
 
-    print.info(f'[!] Performing SYN scan on network {target_network_ip}/{target_network_mask} ({len(target_ips_list)} addresses)')
 
-    target_ports_list = validation.validate_ports(ports=target_ports)
+def run(target_ips: str | list[str], target_ports: str | int | list[int], max_threads: int = 100, timeout: int = 2):
+    target_ips_list = validation.validate_ip_addresses(target_ips)
+    target_ports_list = validation.validate_ports(target_ports)
+
+    if len(target_ips) == 1:
+        print.info(f'[!] Performing SYN scan on {target_ports_list[0]}')
+    else:
+        print.info(f'[!] Performing SYN scan on {len(target_ports_list)} addresses')
 
     cases = [
         {'target_ip': ip, 'target_port': port}
@@ -25,21 +28,18 @@ def run(target_network_ip: str, target_network_mask: int | None = None, target_p
 
     return results
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='syn scan one or many hosts')
-    parser.add_argument('target_network_ip', help='ip address of targetted network')
+    parser.add_argument('target_ips', help='list of target ips (e.g., 192.168.8.1,192.168.8.2,192.168.8.3')
     parser.add_argument('target_ports', help='list of target ports (e.g., 1,2,8-15,22)')
-    parser.add_argument('-m', nargs='?', help='maks of targetted network', type=int, metavar='mask', dest='target_network_mask')
     parser.add_argument('-w', nargs='?', default=1, help='seconds to wait for reply', type=int, metavar='wait', dest='timeout')
     parser.add_argument('-t', nargs='?', default=100, help='number of threads to use', type=int, metavar='threads', dest='max_threads')
     args = parser.parse_args()
 
     results = run(
-        target_network_ip=args.target_network_ip,
+        target_ips=args.target_ips,
         timeout=args.timeout,
         max_threads=args.max_threads,
-        target_ports=args.target_ports,
-        target_network_mask=args.target_network_mask
+        target_ports=args.target_ports
     )
 
